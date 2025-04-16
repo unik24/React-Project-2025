b6065a8601cf49e64178a85a5c813de304f35354
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-04-16 12:18:55 +0200

    #2 Add MovieCard component with like functionality

---
+++ src/components/MovieCard.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const MovieCard = ({ title, url }) => {
+  const [liked, setLiked] = useState(false);
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={url} alt={title} className="movie-poster" />
+        <button
+          className="favorite-btn"
+          onClick={() => {
+            setLiked(!liked);
+          }}
+        >
+          {liked ? "♥" : "♡"}
+        </button>
+      </div>
+      <h3 className="movie-title">{title}</h3>
+    </div>
+  );
+};
