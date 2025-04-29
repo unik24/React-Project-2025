40b95e17e0d47bd8b332b82136e7cc7a7d126830
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-04-29 17:31:31 +0200

    Simplify MovieCard and MovieInfo components to display only poster, title, and like button

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,18 +3,7 @@ import MoviePoster from "../MoviePoster/MoviePoster";
 import MovieInfo from "../MovieInfo/MovieInfo";
 import "./MovieCard.css";
 
-const MovieCard = ({
-  title,
-  url,
-  releaseDate,
-  genre,
-  rating,
-  duration,
-  description,
-  country,
-  liked,
-  onLikeToggle,
-}) => {
+const MovieCard = ({ title, url, liked, onLikeToggle }) => {
   return (
     <div className="movie-card">
       <MoviePoster
@@ -23,15 +12,7 @@ const MovieCard = ({
         liked={liked}
         onLike={onLikeToggle}
       />
-      <MovieInfo
-        title={title}
-        releaseDate={releaseDate}
-        genre={genre}
-        rating={rating}
-        duration={duration}
-        description={description}
-        country={country}
-      />
+      <MovieInfo title={title} />
     </div>
   );
 };
--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,35 +1,11 @@
 import React from "react";
 
-const MovieInfo = ({
-  title,
-  showDetails,
-  releaseDate,
-  genre,
-  rating,
-  duration,
-  description,
-  country,
-}) => {
+const MovieInfo = ({ title }) => {
   return (
     <div className="movie-info">
       <h3>{title}</h3>
-
-      {showDetails && (
-        <>
-          <p className="info-item">
-            <span className="bold">Release Date:</span> {releaseDate}
-          </p>
-          <p className="info-item">
-            <span className="bold">Genre:</span> {genre}
-          </p>
-        </>
-      )}
     </div>
   );
-};
-
-MovieInfo.defaultProps = {
-  showDetails: false,
 };
 
 export default MovieInfo;
