f933844a0d4a82a5a68fc80a92fccfc2581b4ca6
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-05-05 18:32:43 +0200

    Wrap MovieCard in Link for navigation

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,17 +2,20 @@ import React from "react";
 import MoviePoster from "../MoviePoster/MoviePoster";
 import MovieInfo from "../MovieInfo/MovieInfo";
 import styles from "./MovieCard.module.css";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ title, url, liked, onLikeToggle }) => {
   return (
     <div className={styles.card}>
-      <MoviePoster
-        url={url}
-        title={title}
-        liked={liked}
-        onLike={onLikeToggle}
-      />
-      <MovieInfo title={title} />
+      <Link to={`/movies/${title}`} className={styles.linkWrapper}>
+        <MoviePoster
+          url={url}
+          title={title}
+          liked={liked}
+          onLike={onLikeToggle}
+        />
+        <MovieInfo title={title} />
+      </Link>
     </div>
   );
 };
