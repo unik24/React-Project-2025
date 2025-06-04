64d18cc466922e60f7d3812786b9416939693413
AnabellaMeluzzi <animeluzzi@gmail.com>
2025-06-04 03:30:24 +0200

    refactor(favorites): Restrict favorites access to logged-in users only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Homepage/Home";
 import MovieDetail from "./pages/MovieDetail/MovieDetail";
@@ -7,20 +7,47 @@ import Layout from "./components/Layout/Layout/Layout";
 import { Profile } from "./pages/Profile/Profile";
 import Favorites from "./pages/Favorites/Favorites";
 import { HomeResetProvider } from "./context/HomeResetContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import Auth from "./pages/Auth/Auth";
+import { MovieProvider } from "./context/MovieContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <div className="App">
-      <HomeResetProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/movies/:movieId" element={<MovieDetail />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Routes>
-        </Layout>
-      </HomeResetProvider>
+      <AuthProvider>
+        <HomeResetProvider>
+          <MovieProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/movies/:movieId" element={<MovieDetail />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <PrivateRoute>
+                      <Profile />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/favorites"
+                  element={
+                    <PrivateRoute>
+                      <Favorites />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="/login" element={<Auth />} />
+              </Routes>
+            </Layout>
+          </MovieProvider>
+        </HomeResetProvider>
+      </AuthProvider>
     </div>
   );
 }
